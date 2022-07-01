6d472d26e4b5dd2dede9fa34960bb839b7d3a952
itwill05 <ignore4557@gmail.com>
2022-07-01 17:00:28 +0900

    ..

--- src/com/itwill/gc/vo/Faq.java
+++ src/com/itwill/gc/vo/Faq.java
@@ -1,5 +1,67 @@
 package com.itwill.gc.vo;
-
+/*
+ 이름           널?       유형            
+------------ -------- ------------- 
+FAQ_NO       NOT NULL NUMBER(10)    
+FAQ_CATEGORY          VARCHAR2(50)  
+FAQ_TITLE             VARCHAR2(50)  
+FAQ_CONTENT           VARCHAR2(500) 
+ */
 public class Faq {
+	private int faq_no;
+	private String faq_category;
+	private String faq_title;
+	private String faq_content;
+	
+	public Faq() {
+		
+	}
+
+	public Faq(int faq_no, String faq_category, String faq_title, String faq_content) {
+		super();
+		this.faq_no = faq_no;
+		this.faq_category = faq_category;
+		this.faq_title = faq_title;
+		this.faq_content = faq_content;
+	}
+
+	public int getFaq_no() {
+		return faq_no;
+	}
+
+	public void setFaq_no(int faq_no) {
+		this.faq_no = faq_no;
+	}
+
+	public String getFaq_category() {
+		return faq_category;
+	}
+
+	public void setFaq_category(String faq_category) {
+		this.faq_category = faq_category;
+	}
+
+	public String getFaq_title() {
+		return faq_title;
+	}
+
+	public void setFaq_title(String faq_title) {
+		this.faq_title = faq_title;
+	}
+
+	public String getFaq_content() {
+		return faq_content;
+	}
+
+	public void setFaq_content(String faq_content) {
+		this.faq_content = faq_content;
+	}
+
+	@Override
+	public String toString() {
+		return "Faq [faq_no=" + faq_no + ", faq_category=" + faq_category + ", faq_title=" + faq_title
+				+ ", faq_content=" + faq_content + "]";
+	}
+	
 
 }
--- src/com/itwill/gc/vo/Question.java
+++ src/com/itwill/gc/vo/Question.java
@@ -1,5 +1,15 @@
 package com.itwill.gc.vo;
-
+/*
+ 이름                널?       유형            
+----------------- -------- ------------- 
+QUESTION_NO       NOT NULL NUMBER(10)    
+USER_ID                    VARCHAR2(20)  
+QUESTION_TITLE             VARCHAR2(50)  
+QUESTION_CONTENT           VARCHAR2(500) 
+QUESTION_CATE_ONE          VARCHAR2(50)  
+QUESTION_CATE_TWO          VARCHAR2(50)  
+QUESTION_DATE              DATE  
+ */
 public class Question {
 
 }
