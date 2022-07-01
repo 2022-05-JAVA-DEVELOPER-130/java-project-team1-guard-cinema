2074a64f47cba7a2d24b9d539d0927c50a770461
wkqrl1 <wkqrl1@naver.com>
2022-07-01 19:58:07 +0900

    ...

--- src/com/itwill/gc/vo/User.java
+++ src/com/itwill/gc/vo/User.java
@@ -71,4 +71,5 @@ public class User {
 	public String toString() {
 		return "User [userId=" + userId + ", name=" + userName + ", phNum= " +userPhNum+ ", jumin= " +userJumin+ ", email=" + userEmail + ", password= " +userPassword+ "]";
 	}
+	
 }
