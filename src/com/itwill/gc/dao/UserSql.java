d08f967f8309519f68b84f583da07ae0f0a5610b
wkqrl1 <wkqrl1@naver.com>
2022-07-01 17:38:51 +0900

    ..

--- src/com/itwill/gc/dao/UserSql.java
+++ src/com/itwill/gc/dao/UserSql.java
@@ -1,5 +1,21 @@
 package com.itwill.gc.dao;
 
 public class UserSql {
-
+	public static final String INSERT_USER_INFO= 
+	"insert into user_info (user_id, user_name, user_ph_num, user_jumin, user_email, user_password) values(?,?,?,?,?,?)";
+	public static final String UPDATE_USER_PH_NUM=
+	"update user_info set user_ph_num=? where user_id=?";
+	public static final String UPDATE_USER_EMAIL=
+	"update user_info set user_email=? where user_id=?";
+	public static final String UPDATE_USER_PASSWORD=
+	"update user_info set user_password=? where user_id=?";
+	public static final String DELETE_USER_INFO=
+	"delete from user_info where user_id=?";
+	public static final String SELECT_USER_ID=
+	"select * from user_info where user_id=?";
+	public static final String SELECT_USER_ALL=
+	"select * from user_info";
+	
+	
+	
 }
--- src/com/itwill/gc/vo/User.java
+++ src/com/itwill/gc/vo/User.java
@@ -1,5 +1,69 @@
 package com.itwill.gc.vo;
 
 public class User {
+	private String userId;
+	private String userName;
+	private String userPhNum;
+	private String userJumin;
+	private String userEmail;
+	private String userPassword;
 
+	public User(String userId, String userName, String userPhNum, String userJumin, String userEmail,
+			String userPassword) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.userPhNum = userPhNum;
+		this.userJumin = userJumin;
+		this.userEmail = userEmail;
+		this.userPassword = userPassword;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPhNum() {
+		return userPhNum;
+	}
+
+	public void setUserPhNum(String userPhNum) {
+		this.userPhNum = userPhNum;
+	}
+
+	public String getUserJumin() {
+		return userJumin;
+	}
+
+	public void setUserJumin(String userJumin) {
+		this.userJumin = userJumin;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
 }
