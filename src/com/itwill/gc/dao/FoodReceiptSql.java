627795f436d81c831063f485ae1f1f08592425ca
wkqrl1 <wkqrl1@naver.com>
2022-07-04 20:19:12 +0900

    ..

--- src/com/itwill/gc/dao/FoodReceiptSql.java
+++ src/com/itwill/gc/dao/FoodReceiptSql.java
@@ -3,6 +3,7 @@ package com.itwill.gc.dao;
 public class FoodReceiptSql {
 
    
+	
    public final static String FOOD_RECEIPT_SELECT_BY_ID =
          "select * from food_receipt where user_id=?";
    public final static String FOOD_RV_SELECT_BY_NO =
