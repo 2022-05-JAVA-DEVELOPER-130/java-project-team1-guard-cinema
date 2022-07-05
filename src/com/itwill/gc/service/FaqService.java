cf6f081fce2e54d5f855e91dbd3ddba6cc5bab13
chojaeh <vip0103@naver.com>
2022-07-05 10:49:09 +0900

    FaqService, GongjiService

--- src/com/itwill/gc/service/FaqService.java
+++ src/com/itwill/gc/service/FaqService.java
@@ -1,5 +1,26 @@
 package com.itwill.gc.service;
 
+import java.util.List;
+
+import com.itwill.gc.dao.FaqDao;
+import com.itwill.gc.vo.Faq;
+
 public class FaqService {
+
+	private FaqDao faqDao;
+
+	public FaqService() throws Exception {
+		faqDao = new FaqDao();
+	}
+
+	// 선택한 공지사항 보기
+	public Faq selectByNo(int faq_no) throws Exception {
+		return faqDao.selectByNo(faq_no);
+	}
+
+	// 선택한 카테고리의 공지 전체보기
+	public List<Faq> selectAllByCate(String faq_category) throws Exception {
+		return faqDao.selectAllByCate(faq_category);
+	}
 
 }
