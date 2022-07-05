90d18d6bbb5be2962397caf5d3f92c9cd36cd6e4
chojaeh <vip0103@naver.com>
2022-07-05 15:52:36 +0900

    FaqServiceTest, GongjiServiceTest

--- src/com/itwill/gc/servicetest/FaqServiceTest.java
+++ src/com/itwill/gc/servicetest/FaqServiceTest.java
@@ -1,5 +1,15 @@
 package com.itwill.gc.servicetest;
 
+import com.itwill.gc.service.FaqService;
+
 public class FaqServiceTest {
 
+	public static void main(String[] args) throws Exception {
+		FaqService faqService = new FaqService();
+		
+		System.out.println("선택한 공지사항 보기");
+		System.out.println(">> "+faqService.selectByNo(1));
+		System.out.println("선택한 카테고리의 공지 전체보기");
+		System.out.println(">> "+faqService.selectAllByCate("카테고리1"));
+	}
 }
