0006b0e7649d797a2e4a049e0fafc9930e01226f
성준 <@DESKTOP-R9O5P0Q>
2022-07-05 17:16:45 +0900

    ..

--- src/com/itwill/gc/service/FoodService.java
+++ src/com/itwill/gc/service/FoodService.java
@@ -1,5 +1,29 @@
 package com.itwill.gc.service;
 
+import java.util.List;
+
+import com.itwill.gc.dao.FoodDao;
+import com.itwill.gc.vo.Food;
+
 public class FoodService {
 
+   private FoodDao foodDao;
+   public FoodService()throws Exception{
+      foodDao = new FoodDao();
+   }
+   
+   /*
+    * 상품번호로 검색
+    */
+   public Food selectByFoodNo(int food_code) throws Exception{
+      return foodDao.selectByFoodNo(food_code);
+   }
+   
+   /*
+    * 상품전체 검색
+    */
+   public List<Food> selectAllFood() throws Exception {
+      return foodDao.selectAllFood();
+   }
+   
 }
