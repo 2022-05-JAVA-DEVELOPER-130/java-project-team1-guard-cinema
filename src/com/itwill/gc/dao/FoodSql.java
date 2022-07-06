085dbf9860925eb7a208de4d140c2dcb007fe451
easpop <easpop@gmail.com>
2022-07-06 11:54:07 +0900

    ..

--- src/com/itwill/gc/dao/FoodSql.java
+++ src/com/itwill/gc/dao/FoodSql.java
@@ -2,7 +2,7 @@ package com.itwill.gc.dao;
 
 public class FoodSql {
 	public final static String FOOD_LIST =
-			"select * from food ";
+			"select * from food";
 	public final static String FOOD_SELECT_BY_NO =
 			"select * from food where food_code=?";
 
